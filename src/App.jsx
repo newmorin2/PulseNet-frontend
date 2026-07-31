4fc929460601a87666d556e9488659ebf5235b15
Lenox Javan <lenox.javan@student.moringaschool.com>
2026-07-31 09:27:09 +0300

    Add appointment routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Appointment from './pages/Appointment'
 
 function App() {
-  return <Appointment />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Appointment />} />
+        <Route path="/appointment" element={<Appointment />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
